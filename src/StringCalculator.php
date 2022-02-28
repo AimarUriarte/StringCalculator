b787bec87b512bf1140c58b8b91d85c124d0d926
AimarUriarte <aimaruri01@gmail.com>
2022-02-28 07:53:17 +0000

    empty_string_returns_0

---
+++ src/StringCalculator.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Deg540\PHPTestingBoilerplate;
+
+class StringCalculator
+{
+    public function add(String $number){
+        return 0;
+    }
+
+}
---
+++ tests/StringCalculatorTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Deg540\PHPTestingBoilerplate\Test;
+
+use Deg540\PHPTestingBoilerplate\StringCalculator;
+use PHPUnit\Framework\TestCase;
+
+class StringCalculatorTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function empty_string_returns_0()
+    {
+        $stringCalculator = new StringCalculator();
+
+        $result = $stringCalculator->add("");
+
+        $this->assertEquals(0, $result);
+    }
+
+}
